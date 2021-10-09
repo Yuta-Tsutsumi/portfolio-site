5dc554806c30bf50542c5e715d6219cec0654dc5
堤佑太 <watagasi.789@gmail.com>
2021-10-09 11:34:37 +0900

    inuptタグの追加したが、エラー発生

--- src/components/atoms/TextBox/TextBox.tsx
+++ src/components/atoms/TextBox/TextBox.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const TextBox: React.FC = () => {
-  return <div></div>;
+  return (
+    <div>
+      <input type="text" name="text" value="初期値（任意の値）" />
+    </div>
+  );
 };
 
 export default TextBox;
