05e349081522586d19e0a73162faa074f105ceb8
堤佑太 <watagasi.789@gmail.com>
2021-10-16 18:38:28 +0900

    コードの整理

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import PortfolioLink from "./components/atoms/Portfolio/PortfolioLink";
 import ProfileLink from "./components/atoms/Profile/ProfileLink";
 import ContactLink from "./components/atoms/Contact/ContactLink";
 import Button from "./components/atoms/Button/Button";
-import TextBox from "./components/atoms/TextBox/TextBox";
+import TextBox from "./components/atoms/TextBoxBottom/TextBox";
+import Input from "./components/atoms/Input/Input";
 import "./App.scss";
 
 const App: React.FC = () => {
@@ -15,6 +16,7 @@ const App: React.FC = () => {
       <ProfileLink />
       <ContactLink />
       <Button />
+      <Input />
       <TextBox />
     </div>
   );
