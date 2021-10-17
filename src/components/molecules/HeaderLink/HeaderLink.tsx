26badf07c62b2b4cf3d845cca8e14b1ee84653fd
堤佑太 <watagasi.789@gmail.com>
2021-10-17 18:59:40 +0900

    コンタクトコンポーネントの追加

--- src/components/molecules/HeaderLink/HeaderLink.tsx
+++ src/components/molecules/HeaderLink/HeaderLink.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const HeaderLink: React.FC = () => {
-  return <div className="HeaderLink"></div>;
+  return (
+    // HeaderLinkに対して、「display:flex, justify-content:space-between」
+    <div className="HeaderLink">
+      <div>HOMEリンク</div>
+      <div>
+        その他のリンクコンポーネント１、その他のリンクコンポーネント２、その他のリンクコンポーネント３
+      </div>
+    </div>
+  );
 };
 
 export default HeaderLink;
