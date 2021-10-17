8960c2fd64ecdeb7fd8960e620a7e50832288040
堤佑太 <watagasi.789@gmail.com>
2021-10-17 11:17:08 +0900

    クラス名の変更

--- src/components/atoms/Input/Input.tsx
+++ src/components/atoms/Input/Input.tsx
@@ -4,8 +4,8 @@ import "./Input.scss";
 const Input: React.FC = () => {
   return (
     <div className="TextBox">
-      <input className="TextBoxTop" placeholder="お名前" />
-      <input className="TextBoxTop" placeholder="メールアドレス" />
+      <input className="Text" placeholder="お名前" />
+      <input className="Text" placeholder="メールアドレス" />
     </div>
   );
 };
