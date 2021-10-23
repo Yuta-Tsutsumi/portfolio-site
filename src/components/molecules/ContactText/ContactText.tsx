2cdd1d2cda9cc12f2e4345e8bdcaa56a3a98581b
堤佑太 <watagasi.789@gmail.com>
2021-10-23 09:56:33 +0900

    テキストボックス・テキストエリアの作成

--- src/components/molecules/ContactText/ContactText.tsx
+++ src/components/molecules/ContactText/ContactText.tsx
@@ -1,12 +1,17 @@
 import React from "react";
-import Input from "./components/atoms/Input/Input";
+import Input from "../../atoms/Input/Input";
+import "./ContactText.scss";
 
 const ContactText = () => {
   return (
-    <div>
-      <Input />
-      <textarea className="TextBox" placeholder="お問い合わせ内容" />
-    </div>
+    <>
+      <div>
+        <Input />
+      </div>
+      <div className="TextArea">
+        <textarea className="TextAreaInput" placeholder="お問い合わせ内容" />
+      </div>
+    </>
   );
 };
 export default ContactText;
