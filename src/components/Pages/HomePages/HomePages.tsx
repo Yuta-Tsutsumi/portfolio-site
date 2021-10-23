095547cf890b16b48d29c4730bac37397df0d2df
堤佑太 <watagasi.789@gmail.com>
2021-10-23 09:54:34 +0900

    ホームページ作成

---
+++ src/components/Pages/HomePages/HomePages.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import HeaderLink from "../../molecules/HeaderLink/HeaderLink";
+import "./HomePages.scss";
+
+const HomePages = () => {
+  return (
+    <>
+      <div>
+        <HeaderLink />
+      </div>
+      <div>
+        <h1>Welcome to</h1> \n <h1>my portfolio.</h1>
+      </div>
+    </>
+  );
+};
+
+export default HomePages;
