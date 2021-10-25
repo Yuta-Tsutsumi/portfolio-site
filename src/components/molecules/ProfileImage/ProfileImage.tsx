d638496d44a2a1ab33ffae8ed7460909f293e08b
堤佑太 <watagasi.789@gmail.com>
2021-10-25 22:17:57 +0900

    ファイルの削除

---
+++ src/components/molecules/ProfileImage/ProfileImage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./ProfileImage.scss";
+
+const ProfileImage1 = () => {
+  return (
+    <div>
+      <img src="./profile.jpg" alt="プロフィール写真" />
+    </div>
+  );
+};
+
+export default ProfileImage1;
