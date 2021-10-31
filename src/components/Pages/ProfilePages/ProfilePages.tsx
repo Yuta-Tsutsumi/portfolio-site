19e127670bdfaabe902f207842dd06ec8b4fe904
堤佑太 <watagasi.789@gmail.com>
2021-11-01 00:28:51 +0900

    プロフィールページの作成

---
+++ src/components/Pages/ProfilePages/ProfilePages.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import ProfileImage from "../../molecules/ProfileImage/ProfileImage";
+
+import profileTopImage from "../../../profileTopImage2.jpg";
+
+import "./ProfilePages.scss";
+
+const ProfilePages = () => {
+  return (
+    <>
+      <div className="topImage">
+        <img src={profileTopImage} alt="トップ画像" />
+      </div>
+
+      <div className="middleImage">
+        <ProfileImage />
+      </div>
+
+      <div className="letterBottom">
+        <p>堤 佑太</p>
+        <p>Yuta Tsutsumi</p>
+        <p>1993年12月21日</p>
+        <p>Twitter/GitHub/Qiita</p>
+      </div>
+      <div className="letterProfile">
+        <a href="">Profile</a>
+      </div>
+    </>
+  );
+};
+
+export default ProfilePages;
