a395361003275fa70a0eab29dee8e7c373110957
堤佑太 <watagasi.789@gmail.com>
2021-11-01 00:29:33 +0900

    ポートフォリオページの作成

---
+++ src/components/Pages/PortfolioPages/PortfolioPages.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./PortfolioPages.scss";
+
+const PortfolioPages = () => {
+  return (
+    <div className="PortfolioLetter">
+      <a href="">Portfolio</a>
+    </div>
+  );
+};
+
+export default PortfolioPages;
