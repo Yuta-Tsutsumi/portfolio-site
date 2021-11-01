53d1cbe6a1008f4cfd1e82ea13bc810c9f42eab0
堤佑太 <watagasi.789@gmail.com>
2021-11-01 18:21:35 +0900

    React-router-domをインストールしたのエラー

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
 import React, { Component } from "react";
-import HomeLink from "./components/atoms/Home/HomeLink";
-import PortfolioLink from "./components/atoms/Portfolio/PortfolioLink";
-import ProfileLink from "./components/atoms/Profile/ProfileLink";
-import ContactLink from "./components/atoms/Contact/ContactLink";
-import Button from "./components/atoms/Button/Button";
-import ContactText from "./components/molecules/ContactText/ContactText";
+import { BrowserRouter } from "react-router-dom";
+import HomePages from "./components/Pages/HomePages/HomePages";
+import ContactPages from "./components/Pages/ContactPages/ContactPages";
+import ProfilePages from "./components/Pages/ProfilePages/ProfilePages";
+import PortfolioPages from "./components/Pages/PortfolioPages/PortfolioPages";
+
 import "./App.scss";
 
 const App: React.FC = () => {
   return (
-    <div className="hero">
-      <HomeLink />
-      <PortfolioLink />
-      <ProfileLink />
-      <ContactLink />
-      <Button />
-      <ContactText />
-    </div>
+    <BrowserRouter>
+      <div>
+        <PortfolioPages />
+        <ProfilePages />
+        <HomePages />
+        <ContactPages />
+      </div>
+    </BrowserRouter>
   );
 };
 
