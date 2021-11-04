da9eee2712fc48ec64fc053eb2b86bec0fd58672
堤佑太 <watagasi.789@gmail.com>
2021-11-05 00:13:37 +0900

    react-router-domを入れてファイルに変更を加えたがエラー

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 import HomePages from "./components/Pages/HomePages/HomePages";
 import ContactPages from "./components/Pages/ContactPages/ContactPages";
 import ProfilePages from "./components/Pages/ProfilePages/ProfilePages";
@@ -9,14 +9,38 @@ import styles from "./App.module.scss";
 const App: React.FC = () => {
   return (
     <BrowserRouter>
-      <div>
-        <ProfilePages />
-        <PortfolioPages />
-        <HomePages />
-        <ContactPages />
+      <div className="App">
+        <Link to="/">HomePages</Link>
+        <br />
+        <Link to="/ProfilePages">ProfilePages</Link>
+        <br />
+        <Link to="/PortfolioPages">PortfolioPages</Link>
+        <br />
+        <Link to="/ContactPages">ContactPages</Link>
       </div>
+      <Switch>
+        <Route exact path="/">
+          <HomePages />
+        </Route>
+        <Route path="/ProfilePages">
+          <ProfilePages />
+        </Route>
+        <Route path="/PortfolioPages">
+          <PortfolioPages />
+        </Route>
+        <Route path="/ContactPages">
+          <ContactPages />
+        </Route>
+      </Switch>
     </BrowserRouter>
   );
 };
 
 export default App;
+
+//     <div>
+//       <ProfilePages />
+//       <PortfolioPages />
+//       <HomePages />
+//       <ContactPages />
+//     </div>
