bf3d92029f29f9d639ef4c6573d23f184030d4a5
堤佑太 <watagasi.789@gmail.com>
2021-11-09 01:20:51 +0900

    react-router-domでの画面遷移の設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,26 @@
-import React, { Component } from "react";
-import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import HomePages from "./components/Pages/HomePages/HomePages";
 import ContactPages from "./components/Pages/ContactPages/ContactPages";
 import ProfilePages from "./components/Pages/ProfilePages/ProfilePages";
 import PortfolioPages from "./components/Pages/PortfolioPages/PortfolioPages";
 import styles from "./App.module.scss";
+import HeaderLink from "./components/molecules/HeaderLink/HeaderLink";
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
-      <div className="App">
-        <Link to="/">HomePages</Link>
-        <br />
-        <Link to="/ProfilePages">ProfilePages</Link>
-        <br />
-        <Link to="/PortfolioPages">PortfolioPages</Link>
-        <br />
-        <Link to="/ContactPages">ContactPages</Link>
-      </div>
       <Switch>
         <Route exact path="/">
           <HomePages />
         </Route>
-        <Route path="/ProfilePages">
+        <Route path="/profile">
           <ProfilePages />
         </Route>
-        <Route path="/PortfolioPages">
+        <Route path="/portfolio">
           <PortfolioPages />
         </Route>
-        <Route path="/ContactPages">
+        <Route path="/contact">
           <ContactPages />
         </Route>
       </Switch>
@@ -37,10 +29,3 @@ const App: React.FC = () => {
 };
 
 export default App;
-
-//     <div>
-//       <ProfilePages />
-//       <PortfolioPages />
-//       <HomePages />
-//       <ContactPages />
-//     </div>
