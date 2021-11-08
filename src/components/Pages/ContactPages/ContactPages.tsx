82d176bd31f2b22ea68fa638e6cd8dcc93376892
堤佑太 <watagasi.789@gmail.com>
2021-11-09 01:17:37 +0900

    ヘッダー コンポーネントの挿入

--- src/components/Pages/ContactPages/ContactPages.tsx
+++ src/components/Pages/ContactPages/ContactPages.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Button from "../../atoms/Button/Button";
 import ContactText from "../../molecules/ContactText/ContactText";
+import HeaderLink from "../../molecules/HeaderLink/HeaderLink";
 import styles from "./ContactPages.module.scss";
 
 const ContactPages: React.FC = () => {
   return (
     <div className={styles.ContactPages}>
-      <div className={styles.ContactTextLink}>
-        <a href="">Contact</a>
-      </div>
+      <HeaderLink />
+      <div className={styles.ContactTextLink}>Contact</div>
       <div className={styles.ContactTextType}>
         <ContactText />
       </div>
--- src/components/Pages/PortfolioPages/PortfolioPages.tsx
+++ src/components/Pages/PortfolioPages/PortfolioPages.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import HeaderLink from "../../molecules/HeaderLink/HeaderLink";
 import styles from "./PortfolioPages.module.scss";
 
 const PortfolioPages: React.FC = () => {
   return (
     <div className={styles.PortfolioLetter}>
-      <a href="">Portfolio</a>
+      <HeaderLink />
+      <div>Portfolio</div>
     </div>
   );
 };
