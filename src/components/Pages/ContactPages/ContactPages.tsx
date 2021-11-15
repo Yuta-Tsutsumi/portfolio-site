4966d027ef8d55fd524219005ef1ccd8e4b0525a
堤佑太 <watagasi.789@gmail.com>
2021-11-15 14:53:40 +0900

    お問い合わせページの作成

--- src/components/Pages/ContactPages/ContactPages.tsx
+++ src/components/Pages/ContactPages/ContactPages.tsx
@@ -1,21 +1,57 @@
-import React from "react";
-import Button from "../../atoms/Button/Button";
-import ContactText from "../../molecules/ContactText/ContactText";
+import React, { useState } from "react";
 import HeaderLink from "../../molecules/HeaderLink/HeaderLink";
 import styles from "./ContactPages.module.scss";
 
-const ContactPages: React.FC = () => {
+const ContactPages = () => {
+  const [email, setEmail] = useState<string>("");
+  const [name, setName] = useState<string>("");
+  const [info, setInfo] = useState<string>("");
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    //再読み込みさせない
+    e.preventDefault();
+
+    //email,nameをアラート
+    alert(`名前:  ${name}
+メールアドレス:  ${email}
+お問合わせ内容:  ${info}`);
+
+    //email,name変数を初期化
+    setEmail("");
+    setName("");
+    setInfo("");
+  };
   return (
-    <div className={styles.contactPages}>
-      <HeaderLink />
-      <div className={styles.contactTextLink}>Contact</div>
-      <div>
-        <ContactText />
+    <>
+      <div className={styles.contactPages}>
+        <HeaderLink />
+        <h2>Contact</h2>
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <input
+            className={styles.textBox}
+            type="text"
+            placeholder="名前"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <br />
+          <input
+            className={styles.textBox}
+            type="email"
+            placeholder="メールアドレス"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <br />
+          <textarea
+            className={styles.textAreaInput}
+            placeholder="お問い合わせ内容"
+            onChange={(e) => setInfo(e.target.value)}
+          />
+          <br />
+          <button className={styles.submitButton} type="submit">
+            送信
+          </button>
+        </form>
       </div>
-      <div className={styles.buttonType}>
-        <Button />
-      </div>
-    </div>
+    </>
   );
 };
 
--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Button.module.scss";
 
 const Button: React.FC = () => {
+  const [sendButton, setSendButton] = useState("");
   const onClickAdd = () => {
+    // if (sendButton === "") return;
     alert("送信完了");
   };
   return (
     <div className={styles.button}>
-      <button onClick={onClickAdd}>送信</button>
+      <span onClick={onClickAdd}>送信</span>
     </div>
   );
 };
