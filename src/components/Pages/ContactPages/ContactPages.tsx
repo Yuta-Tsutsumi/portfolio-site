adcdd27e98ba133736b251a93c93370fb6711e91
堤佑太 <watagasi.789@gmail.com>
2021-11-22 18:47:11 +0900

    エラー！

--- src/components/Pages/ContactPages/ContactPages.tsx
+++ src/components/Pages/ContactPages/ContactPages.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import HeaderLink from "../../molecules/HeaderLink/HeaderLink";
+import axios from "axios";
 import styles from "./ContactPages.module.scss";
 
 interface IFormInputs {
@@ -13,11 +14,44 @@ const onSubmit: SubmitHandler<IFormInputs> = (data: any) => {
   console.log(data);
 };
 export default function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
   const {
     register,
     formState: { errors },
     handleSubmit,
   } = useForm<IFormInputs>();
+
+  async function submitForm(e: any) {
+    e.preventDefault();
+    alert("送信完了");
+    const webHookURL =
+      "https://hooks.slack.com/services/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";
+  }
+  const data = {
+    text: `NAME: ${name}\n EMAIL: ${email}\n MESSAGE: ${message}`,
+  };
+
+  let res = axios.post(webHookURL, JSON.stringify(data), {
+    withCredentials: false,
+    transformRequest: [
+      (data, headers) => {
+        delete headers.post["Content-Yype"];
+        return data;
+      },
+    ],
+  });
+
+  if (res.status === 200) {
+    alert("送信完了");
+
+    setName("");
+    setEmail("");
+    setMessage("");
+  } else {
+    alert("送信失敗");
+  }
 
   return (
     <form className={styles.contactPages} onSubmit={handleSubmit(onSubmit)}>
@@ -30,8 +64,13 @@ export default function App() {
           <input
             className={styles.textBox}
             // placeholder="名前"
+            id="name"
             type="text"
             {...register("name", { required: true })}
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
           />
           <label className={styles.label}>name</label>
         </div>
@@ -43,8 +82,13 @@ export default function App() {
           <input
             className={styles.emailBox}
             // placeholder="メールアドレス"
+            id="email"
             type="email"
             {...register("email", { required: true })}
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
           />
           <label className={styles.label}>email</label>
         </div>
@@ -58,7 +102,12 @@ export default function App() {
           <textarea
             className={styles.messageBox}
             // placeholder="お問い合わせ内容"
+            id="message"
             {...register("message", { required: true })}
+            value={message}
+            onChange={(e) => {
+              setMessage(e.target.value);
+            }}
           />
           <label className={styles.label}>message</label>
         </div>
@@ -69,7 +118,11 @@ export default function App() {
         )}
       </div>
       <div className={styles.aaa}>
-        <button className={styles.submitButton} type="submit">
+        <button
+          className={styles.submitButton}
+          type="submit"
+          onClick={(e) => submitForm(e)}
+        >
           送信
         </button>
       </div>
