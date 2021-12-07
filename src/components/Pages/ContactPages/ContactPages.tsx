6fe649a888b86c4d8109fc85114197bc7fb19d90
堤佑太 <watagasi.789@gmail.com>
2021-12-07 23:40:00 +0900

    リファクタリング

--- src/components/Pages/ContactPages/ContactPages.tsx
+++ src/components/Pages/ContactPages/ContactPages.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import HeaderLink from "../../molecules/HeaderLink/HeaderLink";
-import axios from "axios";
 import styles from "./ContactPages.module.scss";
 
 interface IFormInputs {
@@ -20,32 +19,9 @@ export default function App() {
     handleSubmit,
   } = useForm<IFormInputs>();
 
-  const onSubmit: SubmitHandler<IFormInputs> = async (data) => {
+  const onSubmit: SubmitHandler<IFormInputs> = (data) => {
     console.log(data);
-    alert(data.email);
   };
-
-  const webHookURL =
-    "https://hooks.slack.com/services/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";
-
-  // let res = axios.post(webHookURL, JSON.stringify, {
-  //   withCredentials: false,
-  //   transformRequest: [
-  //     (data, headers) => {
-  //       delete headers.post["Content-Yype"];
-  //       return data;
-  //     },
-  //   ],
-  // });
-
-  // if (res.input === 200) {
-  //   alert("送信完了");
-  //   setName("");
-  //   setEmail("");
-  //   setMessage("");
-  // } else {
-  //   alert("送信失敗");
-  // }
 
   return (
     <form className={styles.contactPages} onSubmit={handleSubmit(onSubmit)}>
@@ -66,9 +42,6 @@ export default function App() {
               setName(e.target.value);
             }}
           />
-          {/* <label htmlFor="name" className={styles.label}>
-            Name
-          </label> */}
         </div>
         {errors.name && (
           <div className={styles.errorsMessage}>名前を入れてください</div>
@@ -85,9 +58,6 @@ export default function App() {
               setEmail(e.target.value);
             }}
           />
-          {/* <label htmlFor="email" className={styles.label}>
-            E-mail
-          </label> */}
         </div>
         {errors.email && (
           <div className={styles.errorsMessage}>
@@ -105,9 +75,6 @@ export default function App() {
               setMessage(e.target.value);
             }}
           />
-          {/* <label htmlFor="message" className={styles.label}> */}
-          {/* Message
-          </label> */}
         </div>
         {errors.message && (
           <div className={styles.errorsMessage}>
@@ -123,73 +90,3 @@ export default function App() {
     </form>
   );
 }
-
-// interface IFormInput {
-//   name: string;
-//   email: string;
-//   message: string;
-// }
-
-// const ContactPages: React.FC = () => {
-//   const [email, setEmail] = useState<string>("");
-//   const [name, setName] = useState<string>("");
-//   const [info, setInfo] = useState<string>("");
-
-//   const handleOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-//     //再読み込みさせない
-//     e.preventDefault();
-
-//     //email,nameをアラート
-//     alert(`名前:  ${name}
-// メールアドレス:  ${email}
-// お問合わせ内容:  ${info}`);
-
-//     // email, name変数を初期化;
-//     setEmail("");
-//     setName("");
-//     setInfo("");
-//   };
-
-//   return (
-//     <>
-//       <div className={styles.contactPages}>
-//         <HeaderLink />
-//         <h2>Contact</h2>
-//         <form onSubmit={(e) => handleOnSubmit(e)}>
-//           <div className={styles.inputWrapper}>
-//             <input
-//               className={styles.textBox}
-//               placeholder="名前"
-//               type="text"
-//               onChange={(e) => setName(e.target.value)}
-//             />
-//           </div>
-
-//           <div className={styles.inputWrapper}>
-//             <input
-//               className={styles.emailBox}
-//               placeholder="メールアドレス"
-//               type="email"
-//               onChange={(e) => setEmail(e.target.value)}
-//             />
-//           </div>
-
-//           <div className={styles.inputWrapper}>
-//             <textarea
-//               className={styles.messageBox}
-//               placeholder="お問い合わせ内容"
-//               onChange={(e) => setEmail(e.target.value)}
-//             />
-//             <span className={styles.inputError}></span>
-//           </div>
-
-//           <button className={styles.submitButton} type="submit">
-//             送信
-//           </button>
-//         </form>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default ContactPages;
