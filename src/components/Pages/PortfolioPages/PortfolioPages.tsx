3fb88a28da54b720594c1e2fa7d48cf422658da6
堤佑太 <watagasi.789@gmail.com>
2021-12-13 22:11:09 +0900

    aタグの追加

--- src/components/Pages/PortfolioPages/PortfolioPages.tsx
+++ src/components/Pages/PortfolioPages/PortfolioPages.tsx
@@ -23,6 +23,7 @@ const PortfolioPages: React.FC = () => {
         </div>
         <nav className={styles.letter}>
           <div className={styles.explanation}>
+            <a href="https://main.d20u4ljofm9ii1.amplifyapp.com/">aaa</a>
             <h3>アプリの特徴</h3>
             <p>
               React+TypeScriptを使用し作成したサイトになります。
